6a1f57e1cef6dd4c2e80aaa472fcd9a54e637780
Paulo Santos <psantos@blackducksoftware.com>
2017-01-04 12:28:03 -0500

    IGC-33 Fix the import statement for StringUtils

--- src/main/java/com/blackducksoftware/integration/gradle/DependencyGatherer.java
+++ src/main/java/com/blackducksoftware/integration/gradle/DependencyGatherer.java
@@ -28,7 +28,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.artifacts.ResolvedConfiguration;
