e9b2042d8f0009e06b7e3e88eb08371514cb7382
Ari Kamen <akamen@blackducksoftware.com>
2017-05-24 14:10:43 -0400

    Delete jenkins_pipeline_hub_scan.groovy

--- jenkins_pipeline_hub_scan.groovy
+++
@@ -1,9 +0,0 @@
-node("rubber") {
-   stage ('Run Hub-PackMan'){
-       withEnv(["PATH=${tool 'jdk8'}/bin:${env.PATH}","JAVA_HOME=${tool 'jdk8'}"])
-       {
-           sh 'curl -o hub-packman.sh https://blackducksoftware.github.io/hub-packman/hub-packman.sh'
-           sh 'bash ./hub-packman.sh'
-       }
-   }
-}
